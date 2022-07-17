b9b2ee425af04a883197ee53a6496fa5267ae49f
joice <joydingforever@gmail.com>
2022-07-17 14:29:25 -0700

    Initial commit

---
+++ src/CodeChallenge2_PrintFromArray.java
@@ -0,0 +1,14 @@
+
+class CodeChallenge2_PrintFromArray {
+    public static void main(String[] args) {
+
+    /*
+    Create an array with seven items. Print the last 3 items.
+    */
+        String[] emojis = {"😃", "😀", "😄", "😁", "😆", "😊", "☺️"};
+        System.out.println(emojis[5]);
+        System.out.println(emojis[6]);
+        System.out.println(emojis[4]);
+
+    }
+}
